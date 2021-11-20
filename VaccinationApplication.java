d0b0b79ea38e0b697cbcf4fe2fa4f73cfa61dac5
ManjunathSandadi <90674135+ManjunathSandadi@users.noreply.github.com>
2021-11-20 12:58:57 +0530

    Add files via upload

---
+++ VaccinationApplication.java
@@ -0,0 +1,15 @@
+package com.example.Vaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationApplication.class, args);
+		
+		System.out.println("Spring Backend processing........");
+	}
+
+}
